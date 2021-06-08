0048faaa3a820cc4e712eaac6e76b71374c74807
Sebastian Scher <sscher@know-center.at>
2021-06-08 14:06:08 +0200

    implemented k_matrix as parameter

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ x_prot = np.random.choice([0, 1], size=n, replace=True)
 x2 = alpha_prot * (x_prot - 0.5) + np.random.normal(0, 1, size=n)
 
 s_real = x1 + x2
-
 df = pd.DataFrame({'x1': x1, 'x2': x2, 'x_prot': x_prot, 's_real': s_real})
 
 # model with protected attribute
@@ -22,6 +21,17 @@ df['s_pred_with_xpr'] = model_with_xpr.predict(df[['x1', 'x_prot']])
 # model without protected attribute
 model_base = linear_model.LinearRegression().fit(df[['x1']], df['s_real'])
 df['s_pred_base'] = model_base.predict(df[['x1']])
+
+
+# logistic regression
+def train_model(X, y):
+    return linear_model.LogisticRegression().fit(X, y)
+
+
+def real_decision_function(s_real):
+    """compute real classes based on real skill"""
+    return (s_real > s_real.mean()).astype(int)
+
 
 # sns.lmplot('x1', 'x2', data=df)
 # sns.lmplot('x1', 'x2', data=df, hue='x_prot')
@@ -36,16 +46,10 @@ df['s_pred_base'] = model_base.predict(df[['x1']])
 # plt.tight_layout()
 #
 # # now define classes: class0: s<0, class1: s>0
-df['class'] = (df['s_real'] > 0).astype(int)
-
+df['class'] = real_decision_function(df['s_real'])
 
 # plt.figure()
 # sns.histplot(x='x_prot', hue='class', data=df, multiple="dodge")
-
-
-# logistic regression
-def train_model(X, y):
-    return linear_model.LogisticRegression().fit(X, y)
 
 
 logmodel_with_xpr = train_model(df[['x1', 'x_prot']], df['class'])
@@ -71,19 +75,24 @@ print(f'when flipping the protected attribute of the wrong cases, only {n_wrong_
 print(f'{n_flipped_better} of the wrong cases would thus get a different (=correct) classification if the'
       f'protected attribute is changed ')
 
-
 # define intervention models
+# matrix with intervention parameters for the four cases (real class can be 1 or 2, and real class can be 1 or two)
+# [[real class 1 & pred class 1, real class 1 & pred class 2],
+#  [real class 2 & pred class 1, real class 2 & pred class 2]]
+k_matrix = np.array([[1 / 10, 1 / 10],
+            [1 / 10, 1 / 10]])
 
-def intervention_model(x1, x2, real_class, pred_class):
+
+def intervention_model(x1, x2, real_class, pred_class, k_matrix):
     """
         update features based on intervention models. works with scalars and vectors
     """
-    # class agnostic model
     # we have four parameters, for four cases (real class can be 1 or 2, and real class can be 1 or two)
-    k_rcl1_pcl1 = 2 / 10
-    k_rcl1_pcl2 = 1 / 10
-    k_rcl2_pcl1 = 1 / 10
-    k_rcl2_pcl2 = 4 / 10
+
+    k_rcl1_pcl1 = k_matrix[0, 0]
+    k_rcl1_pcl2 = k_matrix[0, 1]
+    k_rcl2_pcl1 = k_matrix[1, 0]
+    k_rcl2_pcl2 = k_matrix[1, 1]
     x1_max = 2
     x2_max = 2
     # make an array with k values for the correct real_class - pred_class combination, then we can use
@@ -106,10 +115,10 @@ df = df_init.copy()
 
 
 def step_model(df):
-    df['x1'], df['x2'] = intervention_model(df['x1'], df['x2'], df['class'], df['class_pred'])
+    df['x1'], df['x2'] = intervention_model(df['x1'], df['x2'], df['class'], df['class_pred'], k_matrix)
     # compute new real classes
     df['s_real'] = df['x1'] + df['x2']
-    df['class'] = (df['s_real'] > df['s_real'].mean()).astype(int)
+    df['class'] = real_decision_function(df['s_real'])
     pred_model = train_model(df[['x1', 'x_prot']], df['class'])
     df['class_pred'] = pred_model.predict(df[['x1', 'x_prot']])
 
@@ -147,3 +156,4 @@ ax = plt.subplot(223)
 res_summary[['s_mean', 's_group1', 's_group2']].plot(alpha=0.8, ax=ax)
 ax = plt.subplot(224)
 res_summary[['coef2']].plot(alpha=0.8, ax=ax)
+plt.suptitle(f'k={k_matrix}')
