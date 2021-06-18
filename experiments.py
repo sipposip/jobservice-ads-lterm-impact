7a2061e4c63d54009adafd165e875a6ad128beaa
Sebastian Scher <sscher@know-center.at>
2021-06-18 15:31:48 +0200

    removed redundant labor_market_type because this can be controlled purely by alpha_lb

--- experiments.py
+++ experiments.py
@@ -28,7 +28,7 @@ def savefig(fname):
     plt.savefig(plotdir + fname + '.svg')
 
 
-def initialize_data(alpha_prot, maxval, labor_market_type):
+def initialize_data(alpha_prot, maxval, alpha_lb):
     """
     generate the initial dataframe
     :param alpha_prot: how much alpha_prot influences x2
@@ -46,17 +46,14 @@ def initialize_data(alpha_prot, maxval, labor_market_type):
         x1 = stats.norm.rvs(-maxval, maxval, size=n)
         x2 = 1 / 2 * (alpha_prot * (x_prot - 0.5) + stats.truncnorm.rvs(-maxval, maxval, size=n))
     # the real skill is simply the mean of x1 and x2
-    s_real, s_eff = compute_skill(x1, x2, x_prot, labor_market_type)
+    s_real, s_eff = compute_skill(x1, x2, x_prot, alpha_lb)
     df = pd.DataFrame({'x1': x1, 'x2': x2, 'x_prot': x_prot, 's_real': s_real, 's_eff':s_eff})
     return df
 
 
-def compute_skill(x1, x2, x_prot, labor_market_type):
+def compute_skill(x1, x2, x_prot, alpha_lb):
     s_real = (x1 + x2) / 2
-    if labor_market_type == 'unbiased':
-        s_eff = s_real
-    elif labor_market_type == 'biased':
-        s_eff = s_real + alpha_lb * (x_prot - 0.5)
+    s_eff = s_real + alpha_lb * 2* (x_prot - 0.5) # factor to so that we get -1 and 1 from x_prot
     return s_real, s_eff
 
 
@@ -130,21 +127,21 @@ def intervention_model(x1, x2, real_class, pred_class, k_matrix):
     return x1_new, x2_new
 
 
-def step_model(df, k_matrix, modeltype, labor_market_type):
+def step_model(df, k_matrix, modeltype, alpha_lb):
     """make one step with the complete model (prediction model plus intervention model)
     and update the dataframe"""
     df['x1'], df['x2'] = intervention_model(df['x1'], df['x2'], df['class'], df['class_pred'], k_matrix)
     # compute new real classes
-    s_real,s_eff = compute_skill(df['x1'], df['x2'], df['x_prot'], labor_market_type)
+    s_real,s_eff = compute_skill(df['x1'], df['x2'], df['x_prot'], alpha_lb)
     df['s_real'] = s_real
-    df['s+eff'] = s_eff
+    df['s_eff'] = s_eff
     df['class'] = real_decision_function(df)
     df['class_pred'], coefs = train_and_predict(df, modeltype)
 
     return df.copy(), coefs
 
 
-def run_experiment(df_init, n_steps, k_matrix, modeltype, labor_market_type, plot=False):
+def run_experiment(df_init, n_steps, k_matrix, modeltype, alpha_lb, plot=False):
     res = []
     res_summary = []
     df = df_init.copy()
@@ -154,7 +151,7 @@ def run_experiment(df_init, n_steps, k_matrix, modeltype, labor_market_type, plo
     df['class_pred'], _ = train_and_predict(df, modeltype)
     df_init = df  # for plotting later on
     for i in range(n_steps):
-        df, coefs = step_model(df, k_matrix, modeltype, labor_market_type)
+        df, coefs = step_model(df, k_matrix, modeltype, alpha_lb)
         summary = pd.DataFrame({  # 'step':i,
             'x1_mean': df['x1'].mean(),
             'x2_mean': df['x2'].mean(),
@@ -210,10 +207,9 @@ n = 1000
 alpha_prot = 2  # influence of alpha_prot on x2
 maxval = 2
 n_steps = 100
-alpha_lb = 1/4
-labor_market_type = 'biased'  # attention: if this is changed ,the data needs to be initialized again!
+alpha_lb = 0 # attention: if this is changed ,the data needs to be initialized again!
 decision_function = 'const'  # 'const' | 'adaptive'
-df_init = initialize_data(alpha_prot=alpha_prot, maxval=maxval, labor_market_type=labor_market_type)
+df_init = initialize_data(alpha_prot=alpha_prot, maxval=maxval, alpha_lb=alpha_lb)
 p = sns.jointplot(x='x1', y='x2', data=df_init, hue='x_prot')
 savefig(f'initial_data_alpha_prot{alpha_prot}_maxval{maxval}')
 sns.displot(df_init, x='s_real', hue='x_prot')
@@ -271,7 +267,7 @@ for config in configs:
     _res = {}
     for modeltype in 'full', 'base':
         res_summary, res = run_experiment(df_init, n_steps=n_steps, k_matrix=config['k_matrix'], modeltype=modeltype,
-                                          labor_market_type=labor_market_type)
+                                          alpha_lb=alpha_lb)
         _res[modeltype] = {**config, 'res_summary': res_summary,
                            'res': res}
 
@@ -303,6 +299,7 @@ for i, scenario in enumerate(exp_results):
 plt.legend()
 plt.ylabel('fraction of unprivileged \n group classified in better class')
 sns.despine()
-savefig(f'main_results_simple_model_{decision_function}')
+plt.suptitle(r'$\alpha_{pr}=$'+str(alpha_prot)+r' $\alpha_{lb}=$'+str(alpha_lb)+' modeltype='+modeltype)
+savefig(f'main_results_simple_model_{decision_function}_alpha_lb{alpha_lb}')
 
 # TODO: compute and plot the time it takes for s_group2 to reach 1 (in the constant decision function case)
